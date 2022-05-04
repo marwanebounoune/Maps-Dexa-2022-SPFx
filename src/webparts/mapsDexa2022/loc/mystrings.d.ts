0d8b7928057f6bc39f444b74f00bd5d6904a44fa
marwanebounoune <61358019+marwanebounoune@users.noreply.github.com>
2022-05-04 10:25:57 +0000

    frist commit

---
+++ src/webparts/mapsDexa2022/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IMapsDexa2022WebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'MapsDexa2022WebPartStrings' {
+  const strings: IMapsDexa2022WebPartStrings;
+  export = strings;
+}
